dea135cd56c2ca3d764c6e22a7c81a1ce37a00d7
vankhoapham <VanKhoa.Pham@georgebrown.ca>
2019-03-15 15:12:40 -0400

    pre alpha

---
+++ Assets/My Assets/Scripts/Interaction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Collision Detected!");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/My Assets/Scripts/NavController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavController : MonoBehaviour
+{
+
+    NavMeshAgent agent;
+    public Transform dest;
+    public Transform start;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        
+    }
+
+    private void Update()
+    {
+        agent.destination = dest.position;
+
+        if (agent.destination == dest.position)
+        {
+            agent.destination = start.position;
+        }
+    }
+}
